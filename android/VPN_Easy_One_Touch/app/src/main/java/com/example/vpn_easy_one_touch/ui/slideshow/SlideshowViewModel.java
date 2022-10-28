d42b39aa498ef13902f57337020f5ad10291a68d
NguyenVen <nguyenven1999@gmail.com>
2022-10-28 21:28:31 +0700

    new

---
+++ android/VPN_Easy_One_Touch/app/src/main/java/com/example/vpn_easy_one_touch/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package com.example.vpn_easy_one_touch.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
