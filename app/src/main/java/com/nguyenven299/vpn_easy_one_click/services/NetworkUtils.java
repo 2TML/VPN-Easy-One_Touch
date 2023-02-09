8a3ae15bdda7f1375df95f6bb8c219f29b365e59
Trần Nguyên Vẹn <49260909+nguyenven299@users.noreply.github.com>
2023-02-09 16:51:07 +0700

    [Core] Update core vpn

---
+++ app/src/main/java/com/nguyenven299/vpn_easy_one_click/services/NetworkUtils.java
@@ -0,0 +1,96 @@
+package com.nguyenven299.vpn_easy_one_click.services;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+
+import java.util.List;
+
+public class NetworkUtils {
+    public static boolean validIP(String ip) {
+        try {
+            if (ip == null || ip.isEmpty()) {
+                return false;
+            }
+
+            String[] parts = ip.split("\\.");
+            if (parts.length != 4) {
+                return false;
+            }
+
+            for (String s : parts) {
+                int i = Integer.parseInt(s);
+                if ((i < 0) || (i > 255)) {
+                    return false;
+                }
+            }
+            if (ip.endsWith(".")) {
+                return false;
+            }
+
+            return true;
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+    }
+
+    public static final int TYPE_WIFI = 1;
+    public static final int TYPE_MOBILE = 2;
+    public static final int TYPE_NOT_CONNECTED = 0;
+    public static final int NETWORK_STATUS_NOT_CONNECTED = 0;
+    public static final int NETWORK_STATUS_WIFI = 1;
+    public static final int NETWORK_STATUS_MOBILE = 2;
+
+    public static int getConnectivityStatus(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        if (null != activeNetwork) {
+            if (activeNetwork.getType() == ConnectivityManager.TYPE_WIFI)
+                return TYPE_WIFI;
+
+            if (activeNetwork.getType() == ConnectivityManager.TYPE_MOBILE)
+                return TYPE_MOBILE;
+        }
+        return TYPE_NOT_CONNECTED;
+    }
+
+    public static int getConnectivityStatusString(Context context) {
+        int conn = getConnectivityStatus(context);
+        int status = 0;
+        if (conn == TYPE_WIFI) {
+            status = NETWORK_STATUS_WIFI;
+        } else if (conn == TYPE_MOBILE) {
+            status = NETWORK_STATUS_MOBILE;
+        } else if (conn == TYPE_NOT_CONNECTED) {
+            status = NETWORK_STATUS_NOT_CONNECTED;
+        }
+        return status;
+    }
+
+    public static String findSSIDForWifiInfo(WifiManager manager, WifiInfo wifiInfo) {
+
+        List<WifiConfiguration> listOfConfigurations = manager.getConfiguredNetworks();
+
+        for (int index = 0; index < listOfConfigurations.size(); index++) {
+            WifiConfiguration configuration = listOfConfigurations.get(index);
+            if (configuration.networkId == wifiInfo.getNetworkId()) {
+                return configuration.SSID;
+            }
+        }
+
+        return "<unknown ssid>";
+    }
+
+    public static boolean isNetworkAvailable(Context context) {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+
+}
---
+++ app/src/main/java/com/nguyenven299/vpn_easy_one_click/services/VpnServices.java
@@ -0,0 +1,6 @@
+package com.nguyenven299.vpn_easy_one_click.services;
+
+import android.net.VpnService;
+
+public class VpnServices extends VpnService {
+}
